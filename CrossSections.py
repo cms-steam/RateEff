04a89f57dda9960c43cad35383bbf707c958d52d
Inga Bucinskaite <ingabu@fnal.gov>
2014-07-31 16:10:00 -0500

    Cross sections were updated in McM

--- CrossSections.py
+++ CrossSections.py
@@ -34,34 +34,34 @@ crossSections8TeV={
 
 ## Use this list if you ran hlt_filter.py when making some of your ntuples.
 crossSections13TeV={
-    ## QCD Cross sections
-    #1st number is the cross section, the last number is EM anti-filter efficiency
-    #for pt bins 5-170, which you can get using efficiency.py
-    #'QCD5to10'    :(80710000000.*0.999999,"QCD_Pt-5to10_antiEMOut_"),  
-    #'QCD10to15'    :(7528000000.*0.9999,"QCD_Pt-10to15_antiEMOut_"),
-    #'QCD15to30'    :(1644000000.*0.9959,"QCD_Pt-15to30_antiEMOut_"),
-    'QCD30to50'    :(164400000.*0.9481,"QCD_Pt-30to50_antiEMOut_"),
-    'QCD50to80'    :(21930000.*0.8820,"QCD_Pt-50to80_antiEMOut_"),
-    'QCD80to120'   :(3000000.*0.8456,"QCD_Pt-80to120_antiEMOut_"),
+#    ## QCD Cross sections
+#    #1st number is the cross section, the last number is EM anti-filter efficiency
+#    #for pt bins 5-170, which you can get using efficiency.py
+#    #'QCD5to10'    :(80710000000.*0.999999,"QCD_Pt-5to10_antiEMOut_"),  
+#    #'QCD10to15'    :(7528000000.*0.9999,"QCD_Pt-10to15_antiEMOut_"),
+#    #'QCD15to30'    :(2237000000.*0.9959,"QCD_Pt-15to30_antiEMOut_"),
+    'QCD30to50'    :(161500000.*0.9481,"QCD_Pt-30to50_antiEMOut_"),
+    'QCD50to80'    :(22110000.*0.8820,"QCD_Pt-50to80_antiEMOut_"),
+    'QCD80to120'   :(3000114.3*0.8456,"QCD_Pt-80to120_antiEMOut_"),
     'QCD120to170'  :(493200.*0.8355,"QCD_Pt-120to170_antiEMOut_"),
-    'QCD170to300'  :(12030.,"QCD_Pt-170to300_nofiltOut_"),
+    'QCD170to300'  :(120300.,"QCD_Pt-170to300_nofiltOut_"),
     'QCD300to470'  :(7475.,"QCD_Pt-300to470_nofiltOut_"),
     'QCD470to600'  :(587.1,"QCD_Pt-470to600_nofiltOut_"),
     'QCD600to800'  :(167.,"QCD_Pt-600to800_nofiltOut_"),
     'QCD800to1000' :(28.25,"QCD_Pt-800to1000_nofiltOut_"),
-    'QCD1000to1400':(8.975,"QCD_Pt-1000to1400_nofiltOut_"),
-    'QCD1400to1800':(0.8975,"QCD_Pt-1400to1800_nofiltOut_"),
-    'QCD1800'      :(0.737844,"QCD_Pt-1800_nofiltOut_"),
-     #EM Enriched cross sections    
-    #the 2nd number is the EM filtering efficiency you can get from McM website
-    #'EMEnr5to10'    :(80710000000.*0.024,"QCD_Pt-5to10_EMEnrichedOut_"),
-    #'EMEnr10to20'    :(8838000000.*0.143,"QCD_Pt-10to20_EMEnrichedOut_"),
+    'QCD1000to1400':(8.195,"QCD_Pt-1000to1400_nofiltOut_"),
+    'QCD1400to1800':(0.7346,"QCD_Pt-1400to1800_nofiltOut_"),
+#    'QCD1800'      :(0.1091,"QCD_Pt-1800_nofiltOut_"),
+#     #EM Enriched cross sections    
+#    #the 2nd number is the EM filtering efficiency you can get from McM website
+#    #'EMEnr5to10'    :(80710000000.*0.024,"QCD_Pt-5to10_EMEnrichedOut_"),
+#    #'EMEnr10to20'    :(8838000000.*0.143,"QCD_Pt-10to20_EMEnrichedOut_"),
     'EMEnr20to30'    :(677300000.*0.007,"QCD_Pt-20to30_EMEnrichedOut_"),
     'EMEnr30to80'    :(185900000.*0.056,"QCD_Pt-30to80_EMEnrichedOut_"),
     'EMEnr80to170'   :(3529000.*0.158,"QCD_Pt-80to170_EMEnrichedOut_"),
-     #Mu Enriched cross sections
-    #the 2nd number is the MuEnriched filtering efficiency you can get from McM website
-    'MuEnr15to20'    :(1576000000.*0.0039*0.9991,"QCD_Pt-15to20_MuEnrichedPt5_antiEMOut_"),
+#     #Mu Enriched cross sections
+#    #the 2nd number is the MuEnriched filtering efficiency you can get from McM website
+    #'MuEnr15to20'    :(1576000000.*0.0039*0.9991,"QCD_Pt-15to20_MuEnrichedPt5_antiEMOut_"),
     'MuEnr20to30'    :(675300000.*0.0065*0.9922,"QCD_Pt-20to30_MuEnrichedPt5_antiEMOut_"),
     'MuEnr30to50'    :(164400000.*0.0122*0.9606,"QCD_Pt-30to50_MuEnrichedPt5_antiEMOut_"),
     'MuEnr50to80'    :(21930000.*0.0218*0.9074,"QCD_Pt-50to80_MuEnrichedPt5_antiEMOut_"),
@@ -73,41 +73,39 @@ crossSections13TeV={
     #'MuEnr600to800'   :(167.*0.0996,"QCD_Pt-600to800_MuEnrichedPt5_nofiltOut_"),
     #'MuEnr800to1000'   :(28.25*0.1033,"QCD_Pt-800to1000_MuEnrichedPt5_nofiltOut_"),
     #'MuEnr1000'   :(8.975*0.1097,"QCD_Pt-1000_MuEnrichedPt5_nofiltOut_"),
-     #W and Z cross sections
+#     #W and Z cross sections
     'WToENu'         :(16000.,"WToENuOut_"),
     'WToMuNu'        :(16100.,"WToMuNuOut_"),
     'ZToMuMu'        :(6870.,"DYToMuMuOut_"),
     'ZToEE'          :(6960.,"DYToEEOut_"),
-    # Photon Cross Section (Flat. Weights already applied by IB)
-    # 'G_Pt-15to3000'  :(2.064133E7,"PUPhoton")
-    
-    }
-
-## Use this list if you ran hlt_nofilter.py when making all of your ntuples and
-## you don't plan to combine QCD MC with lepton enriched QCD MC.
+#    # Photon Cross Section (Flat. Weights already applied by IB)
+#    # 'G_Pt-15to3000'  :(2.064133E7,"PUPhoton")
+#    
+}
+#
+## Use this list if you only ran hlt_nofilter.py when making some of your ntuples.
 #crossSections13TeV={
 #    ## QCD Cross sections
-#    'QCD5to10'    :(80710000000.,"QCD_Pt-5to10_antiEMOut_"),  
-#    'QCD10to15'    :(7528000000.,"QCD_Pt-10to15_antiEMOut_"),
-#    'QCD15to30'    :(1644000000.,"QCD_Pt-15to30_antiEMOut_"),
-#    'QCD30to50'    :(164400000.,"QCD_Pt-30to50_antiEMOut_"),
-#    'QCD50to80'    :(21930000.,"QCD_Pt-50to80_antiEMOut_"),
-#    'QCD80to120'   :(3000000.,"QCD_Pt-80to120_antiEMOut_"),
+#    #1st number is the cross section, the last number is EM anti-filter efficiency
+#    #for pt bins 5-170, which you can get using efficiency.py
+#    #'QCD5to10'    :(80710000000.,"QCD_Pt-5to10_antiEMOut_"),  
+#    #'QCD10to15'    :(7528000000.,"QCD_Pt-10to15_antiEMOut_"),
+#    #'QCD15to30'    :(2237000000.,"QCD_Pt-15to30_antiEMOut_"),
+#    'QCD30to50'    :(161500000.,"QCD_Pt-30to50_antiEMOut_"),
+#    'QCD50to80'    :(22110000.,"QCD_Pt-50to80_antiEMOut_"),
+#    'QCD80to120'   :(3000114.3.,"QCD_Pt-80to120_antiEMOut_"),
 #    'QCD120to170'  :(493200.,"QCD_Pt-120to170_antiEMOut_"),
-#    'QCD170to300'  :(12030.,"QCD_Pt-170to300_nofiltOut_"),
+#    'QCD170to300'  :(120300.,"QCD_Pt-170to300_nofiltOut_"),
 #    'QCD300to470'  :(7475.,"QCD_Pt-300to470_nofiltOut_"),
 #    'QCD470to600'  :(587.1,"QCD_Pt-470to600_nofiltOut_"),
 #    'QCD600to800'  :(167.,"QCD_Pt-600to800_nofiltOut_"),
 #    'QCD800to1000' :(28.25,"QCD_Pt-800to1000_nofiltOut_"),
-#    'QCD1000to1400':(8.975,"QCD_Pt-1000to1400_nofiltOut_"),
-#    'QCD1400to1800':(0.8975,"QCD_Pt-1400to1800_nofiltOut_"),
-#    'QCD1800'      :(0.737844,"QCD_Pt-1800_nofiltOut_"),
-#     #W and Z cross sections
+#    'QCD1000to1400':(8.195,"QCD_Pt-1000to1400_nofiltOut_"),
+#    'QCD1400to1800':(0.7346,"QCD_Pt-1400to1800_nofiltOut_"),
+#    'QCD1800'      :(0.1091,"QCD_Pt-1800_nofiltOut_"),
+##     #W and Z cross sections
 #    'WToENu'         :(16000.,"WToENuOut_"),
 #    'WToMuNu'        :(16100.,"WToMuNuOut_"),
 #    'ZToMuMu'        :(6870.,"DYToMuMuOut_"),
 #    'ZToEE'          :(6960.,"DYToEEOut_"),
-#    # Photon Cross Section (Flat. Weights already applied by IB)
-#    # 'G_Pt-15to3000'  :(2.064133E7,"PUPhoton")
-#    
-#    }
+##   }
